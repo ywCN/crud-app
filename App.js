90bdcd90e01da0e47ff12ce6eb6816b599a9ef58
ywCN <wangyouhao90@gmail.com>
2018-01-20 19:40:09 -0500

    connect to firebase

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import firebase from 'firebase';
 
 import reducers from './reducers';
 import AddScreen from './screens/AddScreen';
@@ -12,6 +13,18 @@ import ListScreen from './screens/ListScreen';
 import ViewScreen from './screens/ViewScreen';
 
 export default class App extends React.Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyCk0lbxBpymCQt60u0FCQyT2Sj-wnKR_xI',
+            authDomain: 'simple-react-firebase.firebaseapp.com',
+            databaseURL: 'https://simple-react-firebase.firebaseio.com',
+            projectId: 'simple-react-firebase',
+            storageBucket: 'simple-react-firebase.appspot.com',
+            messagingSenderId: '772412300021'
+        };
+
+        firebase.initializeApp(config);
+    }
     render() {
         const store = createStore(reducers, {}, applyMiddleware(thunk));
         const MainNavigator = StackNavigator(
